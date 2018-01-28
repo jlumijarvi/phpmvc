0d6d158cfb7a0573ba267d3a86dba2570bfa2856
Janne Lumijärvi <janne.kr.lumijarvi@gmail.com>
2018-01-28 18:02:53 -0500

    Added config.default

---
+++ config/config.default.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'debug' => false,
+    'tidy' => false && extension_loaded('tidy'),
+    'db' => [
+        'host' => '',
+        'db' => '',
+        'user' => '',
+        'pass' => ''
+    ]
+];
